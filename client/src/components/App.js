0e5e45a043fa3ca64f907279de2968603d60cdb3
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2018-11-16 12:42:40 +0000

    feat: react with bulma client simple setup

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <section className="hero is-info is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                Hello to React GraphQL Client
+              </h1>
+              <h2 className="subtitle">
+                Primary subtitle
+              </h2>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
