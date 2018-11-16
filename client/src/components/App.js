75e0208c5887f339f81a02fd41e48b15705aaf02
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2018-11-16 13:04:18 +0000

    feat: Setup apollo client in App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+
 import './App.css';
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <section className="hero is-info is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">
-                Hello to React GraphQL Client
-              </h1>
-              <h2 className="subtitle">
-                Primary subtitle
-              </h2>
+      <ApolloProvider client={client}>
+        <div>
+          <section className="hero is-info is-bold">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Hello to React, Apollo and GraphQL Client
+                </h1>
+                <h2 className="subtitle">
+                  Github
+                </h2>
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
+          </section>
+        </div>
+      </ApolloProvider>
     );
   }
 }
