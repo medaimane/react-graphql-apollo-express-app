253864144cdd859d181ba4dffaab5019953014de
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2018-11-16 17:01:35 +0000

    feat: Add Launch component to show items

--- client/src/components/Launches/Launch.js
+++ client/src/components/Launches/Launch.js
@@ -1,16 +1,40 @@
-import React, { Component, Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React, {Fragment} from 'react'
 
-export default class Launch extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
-
-  render() {
+export default function Launch({launch}) {
+    const {
+        // flight_number,
+        mission_name,
+        launch_year,
+        // launch_date_local,
+        // launch_success
+    } = launch;
     return (
-      <Fragment>
-        
-      </Fragment>
+        <Fragment>
+            <div className="card">
+                <div className="card-content">
+                    <p className="title">
+                    “{mission_name}.”
+                    </p>
+                    <div className="subtitle">
+                    <p>{launch_year}</p>
+                    <a href="!#">@bulmaio</a>. <a href="!#">#css</a> <a href="!#">#responsive</a>
+                    <br />
+                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    </div>
+                </div>
+                <footer className="card-footer">
+                    <p className="card-footer-item">
+                    <span>
+                        View on <a href="!#">Twitter</a>
+                    </span>
+                    </p>
+                    <p className="card-footer-item">
+                    <span>
+                        Share on <a href="!#">Facebook</a>
+                    </span>
+                    </p>
+                </footer>
+            </div>
+        </Fragment>
     )
-  }
 }
--- client/src/components/Launches/Launches.js
+++ client/src/components/Launches/Launches.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { Query } from "react-apollo";
 import gql from "graphql-tag"
+import Launch from './Launch';
 
 const LaunchesQuery = gql`
     query LaunchesQuery{
@@ -28,19 +29,27 @@ export class Launches extends Component {
                     if (loading) return <h2 className="subtitle">Loading...</h2>;
                     if (error){ console.log(error); return <h2 className="subtitle">Error :(</h2>;}
         
-                    return data.launches.map(({
-                        flight_number,
-                        mission_name,
-                        launch_year,
-                        launch_date_local,
-                        launch_success
-                    }) => (
-                        <div key={flight_number}>
-                            <div>
-                            <p>{`${flight_number} - ${mission_name}: ${launch_date_local} (${launch_year}) | ${launch_success}`}</p>
-                            </div>
-                        </div>
-                    ));
+                    // return data.launches.map(({
+                    //     flight_number,
+                    //     mission_name,
+                    //     launch_year,
+                    //     launch_date_local,
+                    //     launch_success
+                    // }) => (
+                    //     <div key={flight_number}>
+                    //         <div>
+                    //         <p>{`${flight_number} - ${mission_name}: ${launch_date_local} (${launch_year}) | ${launch_success}`}</p>
+                    //         </div>
+                    //     </div>
+                    // ));
+
+                    return <Fragment>
+                        {
+                            data.launches.map(launch => (
+                                <Launch key={launch.flight_number} launch={launch} />
+                            ))
+                        }
+                    </Fragment>
                 }}
 
             </Query>
